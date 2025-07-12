65fe3533bcccd9929765cdfc868c7a8e69a9daaa
石田貴行 <t.ishida0710@gmail.com>
2025-07-13 01:01:44 +0900

    編集

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // スライダー機能
   // const sliderImages = document.querySelectorAll(".slider-image");
   // const prevButton = document.querySelector(".prev-button");
@@ -45,16 +45,16 @@ document.addEventListener('DOMContentLoaded', () => {
   //   slideInterval = setInterval(nextImage, 5000);
   // });
 
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     setTimeout(() => {
-      const body = document.querySelector('body');
-      body.classList.add('loaded');
+      const body = document.querySelector("body");
+      body.classList.add("loaded");
     }, 4000);
   });
 
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper(".swiper", {
     // スライドの枚数
-    slidesPerView: 3,
+    slidesPerView: 1.5,
     // ループ再生
     loop: true,
     // 中央にスライドを表示
@@ -62,39 +62,39 @@ document.addEventListener('DOMContentLoaded', () => {
     // 自動再生
     autoplay: {
       // 自動再生
-      delay: 3000, // 1秒後に次のスライド（初期値：3000）
+      delay: 5000, // 1秒後に次のスライド（初期値：3000）
       disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
     },
     // 矢印の表示
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
 
   // スクロール時のヘッダーの色の変化（任意）
-  const header = document.querySelector('header');
-  window.addEventListener('scroll', () => {
+  const header = document.querySelector("header");
+  window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
-      header.style.backgroundColor = '#113a25'; /* スクロール後の少し濃い色 */
-      header.style.boxShadow = '0 4px 8px rgba(0,0,0,0.3)';
+      header.style.backgroundColor = "#113a25"; /* スクロール後の少し濃い色 */
+      header.style.boxShadow = "0 4px 8px rgba(0,0,0,0.3)";
     } else {
-      header.style.backgroundColor = '#1a4d2e'; /* 元の色 */
-      header.style.boxShadow = '0 2px 5px rgba(0,0,0,0.2)';
+      header.style.backgroundColor = "#1a4d2e"; /* 元の色 */
+      header.style.boxShadow = "0 2px 5px rgba(0,0,0,0.2)";
     }
   });
 
   // スムーズスクロール
-  document.querySelectorAll('nav a').forEach((anchor) => {
-    anchor.addEventListener('click', function (e) {
+  document.querySelectorAll("nav a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
       e.preventDefault();
 
-      const targetId = this.getAttribute('href');
+      const targetId = this.getAttribute("href");
       const targetElement = document.querySelector(targetId);
 
       window.scrollTo({
         top: targetElement.offsetTop - header.offsetHeight, // ヘッダーの高さを考慮
-        behavior: 'smooth',
+        behavior: "smooth",
       });
     });
   });
